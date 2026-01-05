7738f1206b0f48db6c926eb38a8331a72debebbc
Anton Glukhov <anton.a.glukhov@gmail.com>
2026-01-05 11:15:54 +0100

    set always BatteryFriendly charge type (after reboot)
    Signed-off-by: Anton Glukhov <anton.a.glukhov@gmail.com>

--- Projects/Mynd/src/persistent_storage/kvstorage.h
+++ Projects/Mynd/src/persistent_storage/kvstorage.h
@@ -57,6 +57,12 @@ inline void init()
 template <typename T>
 constexpr std::optional<T> load()
 {
+    if constexpr (std::is_same_v<T, Teufel::Ux::System::ChargeType>)
+    {
+        /* According to the spec, the charge type should be always BatteryFriendly after reboot. */
+        return Teufel::Ux::System::ChargeType::BatteryFriendly;
+    }
+
     if constexpr (std::is_same_v<T, Teufel::Ux::System::BatterySoCAlgoState>)
     {
         uint16_t cell_value;
